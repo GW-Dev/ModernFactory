afc7d88ff5df6cb1418d01e484afb5ffb7be030f
GW-Dev <grantewilliams51@gmail.com>
2025-11-26 13:54:49 -0600

    finish most of early game

--- kubejs/server_scripts/vanilla_changes.js
+++ kubejs/server_scripts/vanilla_changes.js
@@ -4,18 +4,38 @@ ServerEvents.tags('block', event => {
     event.add('minecraft:needs_iron_tool', 'minecraft:iron_ore')
     event.add('minecraft:needs_iron_tool', 'minecraft:deepslate_iron_ore')
 })
+
 ServerEvents.recipes(event => {
     // -- MOD NAMESPACE UTILITY FUNCTIONS -- // 
-    let mf = (id) => `modern_factory:stone_age/ores/${id}`;
+    let mf = (id) => `modern_factory:stone_age/${id}`;
 
     // -- REMOVED RECIPES -- //
     const VANILLA_DELETED_SMELTING = [
         mc('iron_ingot_from_smelting_raw_iron'),
         mc('iron_ingot_from_blasting_raw_iron'),
+        mc('iron_ingot_from_smelting_iron_ore'),
+        mc('iron_ingot_from_blasting_iron_ore'),
+        mc('iron_ingot_from_smelting_deepslate_iron_ore'),
+        mc('iron_ingot_from_blasting_deepslate_iron_ore'),
         mc('copper_ingot_from_smelting_raw_copper'),
         mc('copper_ingot_from_blasting_raw_copper'),
         mc('gold_ingot_from_smelting_raw_gold'),
         mc('gold_ingot_from_blasting_raw_gold'),
+        fb('framing_saw/framed_item_frame'),
+        fb('framing_saw/framed_glowing_item_frame'),
+        th('goggles_arcane_workbench'),
+        th('thavmite_vanguard_helmet_infusion'),
+        th('thavmite_vanguard_chestplate_infusion'),
+        th('thavmite_vanguard_leggings_infusion'),
+        th('thavmite_vanguard_boots_infusion'),
+        es('ender_pouch'),
+        cc('printed_book'),
+        fd('book_from_canvas'),
+        mc('furnace'),
+        mc('paper'),
+        mc('sugar_from_sugar_cane'),
+        fd('paper_from_tree_bark'),
+        su('lunch_basket')
     ];
     VANILLA_DELETED_SMELTING.forEach(id => event.remove( {id: id} ));
 
@@ -67,7 +87,7 @@ ServerEvents.recipes(event => {
         }
 
         mortar(
-            mf(MORTAR_GRINDING_RAW_ORES),
+            mf('ores/' + MORTAR_GRINDING_RAW_ORES),
             8,
             {},
             [ { item: itemId } ],
@@ -77,4 +97,93 @@ ServerEvents.recipes(event => {
             },
         )
     })
+
+        // -- CUSTOM RECIPE UTILITY FUNCTION -- //
+    let drying = (id, cook_time, xp, item_inputs, item_outputs) => {
+        let newRecipe = {
+            type: ed('drying_rack'),
+            cookingtime: cook_time,
+            experience: xp,
+            result: item_outputs
+        }
+        if (item_inputs)
+            newRecipe['ingredient'] = item_inputs;
+        event.custom(newRecipe).id(id);
+    }
+
+    // tanned leather recipe
+    drying(
+        mf('tanned_leather'),
+        2000,
+        10.0,
+        [ {tag: 'c:leathers'} ],
+        {
+            count: 1,
+            id: kj('tanned_leather')
+        }
+    )
+
+    event.smelting(
+        su('ash'),
+        mi('wood_pulp')
+    ).cookingTime(200).id(mf('ash_from_wood_pulp'))
+
+    event.replaceInput(
+        {input: mc('leather')},
+        mc('leather'),
+        kj('tanned_leather')
+    )
+
+    event.replaceInput(
+        {id: hg('glider_wing')},
+        mc('stick'),
+        mi('bronze_rod')
+    )
+
+    event.replaceInput(
+        {id: hg('glider_framework')},
+        mc('iron_ingot'),
+        mi('iron_rod')
+    )
+
+    event.replaceInput(
+        {id: 'morphtool:tool'},
+        mc('iron_ingot'),
+        mi('iron_rod')
+    )
+
+    event.replaceInput(
+        {id: mc('piston')},
+        mc('iron_ingot'),
+        mi('iron_rod')
+    )
+
+    event.replaceInput(
+        {id: mc('glider_framework')},
+        mc('iron_ingot'),
+        mi('bronze_ingot')
+    )
+
+    event.shaped(Item.of(mc('furnace')),
+    [   'AAA',
+        'ABA',
+        'AAA'
+    ],
+    {
+        A: '#minecraft:stone_tool_materials',
+        B: '#c:ingots/bronze'
+
+    }).id(mf('furnace'))
+
+        event.shaped(Item.of(mt('primitive_furnace')),
+    [   'AAA',
+        'ABA',
+        'AAA'
+    ],
+    {
+        A: mc('mud_bricks'),
+        B: mc('campfire')
+
+    }).id(mf('primitive_furnacr'))
+
 })
